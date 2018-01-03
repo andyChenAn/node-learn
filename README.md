d250b34c34287b23596fd2dbd6cd7072b8c11ee6
andy <andy@jobui.com>
2018-01-03 18:18:51 +0800

    feat:添加stream

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 目录
 - [流](https://github.com/andyChenAn/node-learn/tree/master/stream/stream.md)
-  - [Readable(可读流)](https://github.com/andyChenAn/node-learn/tree/master/stream/Readable.md)
+  - [Readable(可读流)](https://github.com/andyChenAn/node-learn/tree/master/stream/readable.md)
---
+++ stream/stream.js
@@ -0,0 +1,28 @@
+// @author by andyChen
+// @date 2017-01-03
+
+
+/* 使用fs.readFile方式来读取文件 */
+console.time();
+console.log('读取数据之前：' + process.memoryUsage().rss);
+fs.readFile('jquery.js' , (err , data) => {
+	console.timeEnd();
+	console.log('读取数据之后：' + process.memoryUsage().rss);
+});
+
+
+/* 使用可读流的方式来读取文件 */
+console.time();
+console.log('读取数据之前：' + process.memoryUsage().rss);
+const readable = fs.createReadStream('jquery.js');
+let m = 0;
+readable.on('data' , data => {
+	if (m == 1) {
+		return;
+	};
+	m++;
+	console.timeEnd();
+});
+readable.on('end' , () => {
+	console.log('读取数据之后：' + process.memoryUsage().rss);
+});
