e8adf805d56a87ab0bf693623c7c7b96943d4ea5
andy <160149808@qq.com>
2018-09-07 15:59:02 +0800

    feat:删除miniExpress代码

--- express/miniExpress/router/layer.js
+++
@@ -1,9 +0,0 @@
-function Layer (path , handle) {
-    this.path = path;
-    this.handle = handle;
-};
-Layer.prototype.handle_request = function (req , res) {
-    let fn = this.handle;
-    fn(req , res);
-};
-module.exports = Layer;
